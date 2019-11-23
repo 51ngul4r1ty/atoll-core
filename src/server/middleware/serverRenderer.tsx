94df4895d0b1c12adca6ca2a6dd311535383ee23
Kevin <kevinbe71@gmail.com>
2019-11-23 08:22:21 -0500

    i18 changes

--- src/server/middleware/serverRenderer.tsx
+++ src/server/middleware/serverRenderer.tsx
@@ -11,6 +11,32 @@ import { layouts } from "@atoll/shared";
 
 const { MainLayout } = layouts;
 
+type Locale = "en_US" | "de_DE" | "default";
+
+const mapAcceptLanguageToLocale = (acceptLanguage: string): Locale => {
+    // something like: en-US,en;q=0.9
+    let language = "";
+    console.log("language: " + acceptLanguage);
+    if (acceptLanguage) {
+        const splitLang = acceptLanguage.split(",");
+        if (splitLang.length) {
+            language = splitLang[0];
+        } else {
+            language = acceptLanguage;
+        }
+    }
+    switch (language) {
+        case "en":
+        case "en-US":
+            return "en_US";
+        case "de":
+        case "de-DE":
+            return "de_DE";
+        default:
+            return "default";
+    }
+};
+
 const serverRenderer: any = () => (req: express.Request & { store: Store }, res: express.Response) => {
     const content = renderToString(
         <Provider store={res.locals.store}>
@@ -24,7 +50,12 @@ const serverRenderer: any = () => (req: express.Request & { store: Store }, res:
         </Provider>
     );
 
-    const state = JSON.stringify(res.locals.store.getState());
+    const oldState = res.locals.store.getState();
+    const locale = mapAcceptLanguageToLocale(req.headers["accept-language"]); // res.locals.language;
+    console.log("locale: " + locale);
+    const newApp = { ...oldState.app, locale };
+    const newState = { ...oldState, app: newApp };
+    const state = JSON.stringify(newState);
 
     return res.send(
         "<!doctype html>" +
@@ -37,6 +68,7 @@ const serverRenderer: any = () => (req: express.Request & { store: Store }, res:
                     ]}
                     scripts={[res.locals.assetPath("bundle.js"), res.locals.assetPath("vendor.js")]}
                     state={state}
+                    language={locale}
                 >
                     {content}
                 </Html>
