5ae5d47288904d11dd9deba72702f902f8cb20fb
Kevin <kevinbe71@gmail.com>
2020-02-24 21:54:46 -0500

    added flopflip

--- src/common/routeBuilder.tsx
+++ src/common/routeBuilder.tsx
@@ -1,21 +1,31 @@
 // externals
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { ConfigureFlopFlip } from "@flopflip/react-redux";
+
+// libraries
+import { flopFlipAdapter } from "@atoll/shared";
 
 // components
 import { IntlProvider, AppContainer, PlanViewContainer, SprintViewContainer, ReviewViewContainer, layouts } from "@atoll/shared";
 
+const appRoutes = (
+    <layouts.MainLayout>
+        <AppContainer>
+            <Switch>
+                <Route path="/" exact component={PlanViewContainer} />
+                <Route path="/plan" exact component={PlanViewContainer} />
+                <Route path="/sprint" exact component={SprintViewContainer} />
+                <Route path="/review" exact component={ReviewViewContainer} />
+            </Switch>
+        </AppContainer>
+    </layouts.MainLayout>
+);
+
 export const buildRoutes = () => (
     <IntlProvider>
-        <layouts.MainLayout>
-            <AppContainer>
-                <Switch>
-                    <Route path="/" exact component={PlanViewContainer} />
-                    <Route path="/plan" exact component={PlanViewContainer} />
-                    <Route path="/sprint" exact component={SprintViewContainer} />
-                    <Route path="/review" exact component={ReviewViewContainer} />
-                </Switch>
-            </AppContainer>
-        </layouts.MainLayout>
+        <ConfigureFlopFlip adapter={flopFlipAdapter} adapterArgs={{ clientSideId: null, user: null }}>
+            {({ isAdapterReady }) => (isAdapterReady ? appRoutes : <div>LOADING...</div>)}
+        </ConfigureFlopFlip>
     </IntlProvider>
 );
