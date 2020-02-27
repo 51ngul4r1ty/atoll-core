9629f55f41907c2b5d814f5ac3f1ab9b99ce811d
Kevin <kevinbe71@gmail.com>
2020-02-27 09:09:17 -0500

    sprint and review tabs behind toggles

--- src/server/api/data/featureToggles.ts
+++ src/server/api/data/featureToggles.ts
@@ -1,8 +1,20 @@
+// NOTE: When adding new entries here manually you can use `npm run output:now`
+
 export const FEATURE_TOGGLE_LIST = {
     /* NOTE: To test browser dark mode prefs on/off just toggle this - it will move to DB later */
     showEditButton: {
-        enabled: true,
+        enabled: false,
         createdDateTime: new Date("2020-02-23T15:55:15.335Z"),
         modifiedDateTime: new Date("2020-02-23T15:55:15.335Z")
+    },
+    enableSprintTab: {
+        enabled: false,
+        createdDateTime: new Date("2020-02-27T13:56:44.341Z"),
+        modifiedDateTime: new Date("2020-02-27T13:56:44.341Z")
+    },
+    enableReviewTab: {
+        enabled: false,
+        createdDateTime: new Date("2020-02-27T13:56:44.341Z"),
+        modifiedDateTime: new Date("2020-02-27T13:56:44.341Z")
     }
 };
