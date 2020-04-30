037aad3ab119bb755f4de560554d766b2c679034
Kevin <kevinbe71@gmail.com>
2020-04-30 00:02:28 -0400

    implemented refresh token

--- src/server/api/handlers/auth.ts
+++ src/server/api/handlers/auth.ts
@@ -1,12 +1,13 @@
 // externals
 import { Request, Response } from "express";
 import * as HttpStatus from "http-status-codes";
+import * as jwt from "jsonwebtoken";
 
 // config
 import { getAuthKey } from "@atoll/shared";
 
 // interfaces/types
-import { ROLE_USER } from "../../types";
+import { ROLE_USER, RefreshTokenContents } from "../../types";
 import { buildAuthToken, buildRefreshToken } from "api/utils/tokenHelper";
 import { getSimpleUuid } from "api/utils/uuidHelper";
 
@@ -43,6 +44,43 @@ export const loginPostHandler = async (req: Request, res: Response) => {
         res.status(HttpStatus.UNAUTHORIZED).send("Either username or password is incorrect");
         return;
     }
+};
+
+export const refreshTokenPostHandler = async (req: Request, res: Response) => {
+    const refreshToken = req.body?.refreshToken;
+    if (!refreshToken) {
+        res.status(HttpStatus.BAD_REQUEST).send("refreshToken is required");
+        return;
+    }
+    const authKey = getAuthKey();
+    if (!authKey) {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send("Invalid configuration - auth key has not been set up");
+        return;
+    }
+    try {
+        let decoded: RefreshTokenContents;
+        try {
+            decoded = jwt.verify(refreshToken, authKey) as RefreshTokenContents;
+        } catch (ex) {
+            return res.status(HttpStatus.FORBIDDEN).send("Invalid refresh token.");
+        }
+
+        res.status(HttpStatus.OK).send({
+            status: HttpStatus.OK,
+            data: {
+                item: {
+                    authToken: buildAuthToken("test-id", decoded.username, ROLE_USER),
+                    refreshToken: buildRefreshToken("test-id", decoded.username, decoded.refreshTokenId)
+                }
+            }
+        });
+        return;
+    } catch (err) {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send("An unknown error occurred while generating an auth token");
+        return;
+    }
+    // res.status(HttpStatus.UNAUTHORIZED).send("Either username or password is incorrect");
+    // return;
 };
 
 export const createUserPostHandler = async (req: Request, res: Response) => {
--- src/server/api/routes.ts
+++ src/server/api/routes.ts
@@ -10,7 +10,7 @@ import { featureTogglesHandler } from "./handlers/featureToggles";
 import { rootHandler } from "./handlers/root";
 import { sprintsHandler } from "./handlers/sprint";
 import { userPreferencesHandler } from "./handlers/userPreferences";
-import { loginPostHandler } from "./handlers/auth";
+import { loginPostHandler, refreshTokenPostHandler } from "./handlers/auth";
 
 export const router = express.Router();
 
@@ -28,3 +28,4 @@ router.post("/backlog-items", auth, backlogItemsPostHandler);
 router.post("/actions/reorder-backlog-items", auth, backlogItemsReorderPostHandler);
 
 router.post("/actions/login", loginPostHandler);
+router.post("/actions/refresh-token", refreshTokenPostHandler);
