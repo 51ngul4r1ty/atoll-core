b13542b7ded75f6854172fc6a7ab58ea67acf0dd
Kevin <kevinbe71@gmail.com>
2020-05-01 08:53:56 -0400

    drop old code

--- src/server/api/handlers/auth.ts
+++ src/server/api/handlers/auth.ts
@@ -80,30 +80,3 @@ export const refreshTokenPostHandler = async (req: Request, res: Response) => {
         return;
     }
 };
-
-// TODO: Finish implementing this
-export const createUserPostHandler = async (req: Request, res: Response) => {
-    res.status(HttpStatus.NOT_IMPLEMENTED).send("Unable to create new user - not implemented yet.");
-    // validate the request body first
-    // const { error } = validate(req.body);
-    // if (error) return res.status(400).send(error.details[0].message);
-
-    // //find an existing user
-    // let user = await User.findOne({ email: req.body.email });
-    // if (user) return res.status(400).send("User already registered.");
-
-    // user = new User({
-    //     name: req.body.name,
-    //     password: req.body.password,
-    //     email: req.body.email
-    // });
-    // user.password = await bcrypt.hash(user.password, 10);
-    // await user.save();
-
-    // const token = user.generateAuthToken();
-    // res.header("x-auth-token", token).send({
-    //     _id: user._id,
-    //     name: user.name,
-    //     email: user.email
-    // });
-};
