82be1770807e64399b875e6054e2e23239b0c63e
Kevin <kevinbe71@gmail.com>
2020-09-15 21:14:30 -0400

    counter model added

---
+++ src/server/dataaccess/models/Counter.ts
@@ -0,0 +1,46 @@
+// externals
+import { Model, DataTypes, Deferrable } from "sequelize";
+
+// data access
+import { sequelize } from "../connection";
+import { BacklogItemModel } from "./BacklogItem";
+
+export class CounterModel extends Model {}
+
+CounterModel.init(
+    {
+        id: {
+            type: DataTypes.STRING(32),
+            primaryKey: true
+        },
+        entity: {
+            type: DataTypes.STRING(32),
+            allowNull: false
+        },
+        entityId: {
+            type: DataTypes.STRING(32),
+            allowNull: true // NOTE: null means that this is the default that applies to all entities
+        },
+        lastNumber: {
+            type: DataTypes.BIGINT,
+            allowNull: true // NOTE: null means that we're going to start at the beginning of the sequence
+        },
+        lastCounterValue: {
+            type: DataTypes.STRING(32), // BIGINT needs 20 digits and we allow a 12 digit prefix
+            allowNull: true
+        }
+    },
+    {
+        modelName: "Counter",
+        freezeTableName: true,
+        paranoid: false,
+        timestamps: true,
+        version: true,
+        sequelize
+    }
+);
+
+CounterModel.belongsTo(BacklogItemModel);
+CounterModel.belongsTo(BacklogItemModel, {
+    as: "nextbacklogitem"
+});
