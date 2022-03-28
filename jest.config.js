62cd2b3696646b45df79776063b0b0d93b349ed7
Kevin Berry <41717340+51ngul4r1ty@users.noreply.github.com>
2022-03-28 09:48:01 -0400

    fix test config

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,7 @@ module.exports = {
     coverageReporters: ["lcov", "text-summary"],
     setupFiles: ["<rootDir>/node_modules/regenerator-runtime/runtime", "<rootDir>/config/polyfills.js"],
     setupFilesAfterEnv: ["<rootDir>config/jest/setup.js"],
-    testMatch: [
-        "<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs,ts,tsx}",
-        "<rootDir>/src/__tests__/**/*.{js,jsx,mjs,ts,tsx}",
-        "<rootDir>/src/**/*.(spec|test).{js,jsx,mjs,ts,tsx}"
-    ],
+    testMatch: ["<rootDir>/src/*.test.{js,jsx,mjs,ts,tsx}", "<rootDir>/src/**/*.test.{js,jsx,mjs,ts,tsx}"],
     testEnvironment: "node",
     testURL: "http://localhost",
     transform: {
