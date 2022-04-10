9679d0d79b6cddd3fb34e524679d0598632ef402
Kevin Berry <41717340+51ngul4r1ty@users.noreply.github.com>
2022-04-10 10:00:36 -0400

    drop TODO

--- src/server/dataaccess/mappers/dataAccessToApiMappers.ts
+++ src/server/dataaccess/mappers/dataAccessToApiMappers.ts
@@ -29,7 +29,7 @@ export const mapDbToApiBacklogItem = (dbItem: any): ApiBacklogItem => {
     return {
         ...dataValueFieldsOnly,
         estimate: storyEstimate,
-        storyEstimate, // TODO: Why do we have both and they map to the same thing??
+        storyEstimate,
         unallocatedPoints,
         unallocatedParts,
         status: dbItem.dataValues.status || "N",
