a8f8d670f1a65005713ba61ca848d7bac76bcc4e
Kevin Berry <41717340+51ngul4r1ty@users.noreply.github.com>
2022-09-15 09:56:18 -0400

    fix for unarchive issue

--- src/server/api/handlers/sprints.ts
+++ src/server/api/handlers/sprints.ts
@@ -87,14 +87,15 @@ export const sprintPatchHandler = async (req: Request, res: Response) => {
         if (!sprint) {
             respondWithNotFound(res, `Unable to find sprint to patch with ID ${queryParamItemId}`);
         } else {
-            const originalSprint = mapDbToApiSprint(sprint);
-            const invalidPatchMessage = getInvalidPatchMessage(originalSprint, body);
+            const invalidPatchMessage = getInvalidPatchMessage(sprint, body);
             if (invalidPatchMessage) {
                 respondWithFailedValidation(res, `Unable to patch: ${invalidPatchMessage}`);
             } else {
-                const newItem = getPatchedItem(originalSprint, body);
-                await sprint.update(mapApiToDbSprint(newItem));
-                respondWithItem(res, sprint, originalSprint);
+                const originalSprint = mapDbToApiSprint(sprint);
+                const newItem = getPatchedItem(sprint, body);
+                await sprint.update(newItem);
+                const apiSprint = mapDbToApiSprint(sprint);
+                respondWithItem(res, apiSprint, originalSprint);
             }
         }
     } catch (err) {
